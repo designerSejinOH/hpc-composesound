6ff796e02ab05ab4812055b1a9e0bfa7f5544744
designerSejinOH <dev@zeroninez.com>
2025-03-29 22:33:36 +0900

    add transformcontrol

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense, useEffect, useRef, useState } from 'react'
 import { Common, View } from '@/components/canvas/View'
-import { Box, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Box, ContactShadows, OrbitControls, PerspectiveCamera, TransformControls, useCursor } from '@react-three/drei'
 import * as THREE from 'three'
+import { useThree } from '@react-three/fiber'
+import { proxy, useSnapshot } from 'valtio'
 
 export default function Page() {
   const [isloading, setIsLoading] = useState(true)
+
+  const mesh = useRef()
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,10 +22,20 @@ export default function Page() {
     <div className='w-screen h-dvh flex flex-col items-center overflow-hidden relative '>
       <View perf className='flex h-full w-full  flex-col items-center justify-center relative'>
         <PerspectiveCamera makeDefault fov={40} position={[0, 0, 10]} near={0.1} far={1000} />
-        <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} maxDistance={100} minDistance={5} />
-        <Box args={[1, 1, 1]} scale={1} position={[0, 0, 0]}>
+        <OrbitControls makeDefault maxPolarAngle={Math.PI / 2.5} minPolarAngle={0} maxDistance={100} minDistance={5} />
+        <Box ref={mesh} args={[1, 1, 1]} scale={1} position={[0, 0, 0]}>
           <meshStandardMaterial color='orange' />
         </Box>
+        <TransformControls object={mesh} mode='translate' />
+        <ContactShadows
+          rotation-x={Math.PI / 2}
+          position={[0, -35, 0]}
+          opacity={0.25}
+          width={200}
+          height={200}
+          blur={1}
+          far={50}
+        />
         <axesHelper args={[1]} scale={1} position={[0, 0, 0]} />
         <gridHelper args={[100, 100]} scale={1} position={[0, 0, 0]} />
       </View>
